affd1b4df4511fddf4e63cf2653c7dc62350f469
Ross Hunter <hunter.dev.87@gmail.com>
2019-05-05 15:01:34 -0500

    sets up browser caching on static files

--- index.js
+++ index.js
@@ -17,17 +17,29 @@ const downloadRoutes = require('./routes/downloadRoutes');
 // Add middlewares
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(`${__dirname}/../portfolio_client/build`));
 
-// routes
+// Serve static assets (i.e. React bundles).  Note that
+// client-side build process handles file versioning,
+// so we can safely cache responses for a long time.
+app.use(express.static(
+	`${__dirname}/../portfolio_client/build`),
+	{maxAge: '30 days'}
+);
+
+// routes.  Note these routes *must* terminate the req/res cycle
+// Otherwise the request will fall through to the wildcard route below,
+// and server will respond with index.html rather than just AJAX data.
 app.use('/api/projects', projectRoutes);
 app.use('/api/contact', emailRoutes);
 app.use('/api/download', downloadRoutes);
 
 
-// Serve bundle
+// Serve built HTML.  We'll cache this as well.
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../portfolio_client/build/index.html'));
+	res.sendFile(
+		path.join(__dirname, '../portfolio_client/build/index.html'),
+		{maxAge: '30 days'}	
+	);
 });
 
 // Handle errors
