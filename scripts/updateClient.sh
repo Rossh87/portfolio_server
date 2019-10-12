34662303c827203c1ba78b3ef3a5c3647e9a1fd3
Ross Hunter <hunter.dev.87@gmail.com>
2019-10-12 18:17:57 -0500

    Update client deployment script
    Client build depends on an env variable to correctly set links to API
    in built code.

--- scripts/updateClient.sh
+++ scripts/updateClient.sh
@@ -4,6 +4,8 @@ export GIT_DIR=$CLIENT_REPO
 
 export GIT_WORK_TREE=$CLIENT_WORK_TREE
 
+export REACT_APP_API_ENDPOINT = 'https://rhunter-dev.com/api'
+
 git pull origin master
 
 yarn --cwd $GIT_WORK_TREE install
@@ -13,5 +15,7 @@ yarn --cwd $GIT_WORK_TREE build
 export -n GIT_DIR
 
 export -n GIT_WORK_TREE
+
+export -n REACT_APP_API_ENDPOINT
 
 
